e807e3944bc08e414c4b40ec8f22815ae3ff525c
Kenny91 <odessa5d4@gmail.com>
2022-01-03 17:48:52 +0900

    Transform update

--- CUDASoftwareRenderer/src/Engine.cu
+++ CUDASoftwareRenderer/src/Engine.cu
@@ -41,14 +41,14 @@ void Engine::Start()
 
 	// Math Test
 	FLOAT4X4 View = Float4x4ViewMatrix(0, 0, 0);
-
+	View._43 = -5.0f;
 	FLOAT4X4 Projection = Float4x4ProjectionMatrix(0.01f, 100.0f, DegreeToRadian(90.0f), 1.777f);
 
-	//XMMATRIX xmProjection = XMMatrixPerspectiveFovLH(DegreeToRadian(90.0f), 1.777f, 0.01f, 100.0f);
+	XMMATRIX xmProjection = XMMatrixPerspectiveFovLH(DegreeToRadian(90.0f), 1.777f, 0.01f, 100.0f);
 
-	FLOAT4 tp0 = FLOAT4(0.0f, 5.0f, -1.0f, 1.0f);
-	FLOAT4 tp1 = FLOAT4(2.5f, 0.0f, -1.0f, 1.0f);
-	FLOAT4 tp2 = FLOAT4(-2.5f, 0.0f, -1.0f, 1.0f);
+	FLOAT4 tp0 = FLOAT4(0.0f, 1.0f, -1.0f, 1.0f);
+	FLOAT4 tp1 = FLOAT4(0.5f, 0.0f, -1.0f, 1.0f);
+	FLOAT4 tp2 = FLOAT4(-0.5f, 0.0f, -1.0f, 1.0f);
 
 	// update from here 22/01/03 2:52 PM
 	tp0 = Float4Multiply(tp0, View);
@@ -59,9 +59,9 @@ void Engine::Start()
 	tp1 = Float4Multiply(tp1, Projection);
 	tp2 = Float4Multiply(tp2, Projection);
 
-	mRenderer->SetPixelNDC(tp0.x, tp0.y, ColorRGBA(1, 1, 1, 1));
-	mRenderer->SetPixelNDC(tp1.x, tp1.y, ColorRGBA(1, 0, 0, 1));
-	mRenderer->SetPixelNDC(tp2.x, tp2.y, ColorRGBA(0, 1, 0, 1));
+	mRenderer->SetPixelNDC(tp0.x/tp0.z, tp0.y/tp0.z, ColorRGBA(1, 1, 1, 1));
+	mRenderer->SetPixelNDC(tp1.x / tp1.z, tp1.y / tp1.z, ColorRGBA(1, 0, 0, 1));
+	mRenderer->SetPixelNDC(tp2.x / tp2.z, tp2.y / tp2.z, ColorRGBA(0, 1, 0, 1));
 
 }
 
