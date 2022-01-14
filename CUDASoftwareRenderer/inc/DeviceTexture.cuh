996113d6218fc75ea974bfb5a8c5a046b6ea6d6f
Kenny91 <odessa5d4@gmail.com>
2022-01-14 13:31:07 +0900

    DeviceTexture update

--- CUDASoftwareRenderer/inc/DeviceTexture.cuh
+++ CUDASoftwareRenderer/inc/DeviceTexture.cuh
@@ -4,7 +4,11 @@ struct DeviceTexture : public DeviceEntity
 {
 	friend struct ResourceManager;
 public:
-	DeviceTexture(void* virtualPtr, size_t size);
+	DeviceTexture(void* virtualPtr, size_t size, unsigned int width, unsigned int height);
+
+	unsigned int GetWidth() const { return mWidth; }
+	unsigned int GetHeight() const { return mHeight; }
+
 	virtual ~DeviceTexture();
 private:
 	unsigned int mWidth;
